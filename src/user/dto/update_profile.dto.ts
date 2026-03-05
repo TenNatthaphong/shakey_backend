5ec331c6836804552cae2190c71aa796810ae6a8
Johnlajohnlajohnny <itdisconnect123@gmail.com>
2026-03-05 21:13:13 +0700

    feat: Implement user profile editing, membership level updates, and point management functionalities.

--- src/user/dto/update_profile.dto.ts
+++ src/user/dto/update_profile.dto.ts
@@ -1,11 +1,29 @@
 import { Type } from "class-transformer";
+import { IsDate, IsEmail, IsOptional, IsString } from "class-validator";
 
 export class EditProfileDto {
+  @IsEmail()
+  @IsOptional()
   email?: string;
+
+  @IsString()
+  @IsOptional()
   username?: string;
+
+  @IsString()
+  @IsOptional()
   firstname?: string;
+
+  @IsString()
+  @IsOptional()
   lastname?: string;
+
+  @IsString()
+  @IsOptional()
   phone?: string;
+
+  @IsDate()
+  @IsOptional()
   @Type(() => Date)
-  birthday?: Date;   
+  birthday?: Date;
 }
